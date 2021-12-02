7eed6f72570a5cd04465fb132ab35968a1be2ee4
Ruslan-MI <64783699+Ruslan-MI@users.noreply.github.com>
2021-12-02 15:18:37 +0300

    Исправлено выравнивание ссылок в футере

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,29 +2,31 @@ import React from "react";
 
 const Footer = () => (
   <footer className="page__footer footer">
-    <ul className="footer__nav-list wrapper">
-      <li className="footer__nav-item">
-        <a className="footer__nav-link" href="#">Корпоративным клиентам</a>
-      </li>
-      <li className="footer__nav-item">
-        <a className="footer__nav-link" href="#">Клиентам</a>
-      </li>
-      <li className="footer__nav-item">
-        <a className="footer__nav-link" href="#">Аренда авто</a>
-      </li>
-      <li className="footer__nav-item">
-        <a className="footer__nav-link" href="#">Каршеринг</a>
-      </li>
-      <li className="footer__nav-item">
-        <a className="footer__nav-link" href="#">Как продать авто</a>
-      </li>
-      <li className="footer__nav-item">
-        <a className="footer__nav-link" href="#">Trade-in</a>
-      </li>
-      <li className="footer__nav-item">
-        <a className="footer__nav-link" href="#">Test drive</a>
-      </li>
-    </ul>
+    <div className="wrapper">
+      <ul className="footer__nav-list">
+        <li className="footer__nav-item">
+          <a className="footer__nav-link" href="#">Корпоративным клиентам</a>
+        </li>
+        <li className="footer__nav-item">
+          <a className="footer__nav-link" href="#">Клиентам</a>
+        </li>
+        <li className="footer__nav-item">
+          <a className="footer__nav-link" href="#">Аренда авто</a>
+        </li>
+        <li className="footer__nav-item">
+          <a className="footer__nav-link" href="#">Каршеринг</a>
+        </li>
+        <li className="footer__nav-item">
+          <a className="footer__nav-link" href="#">Как продать авто</a>
+        </li>
+        <li className="footer__nav-item">
+          <a className="footer__nav-link" href="#">Trade-in</a>
+        </li>
+        <li className="footer__nav-item">
+          <a className="footer__nav-link" href="#">Test drive</a>
+        </li>
+      </ul>
+    </div>
   </footer>
 );
 
